a677f953376843d936125a8585b3f0529edd9ada
Alex Shan <shantong.sir@gmail.com>
2021-10-26 23:03:57 +0800

    build: add `assetPrefix` in next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
+const isProduction = process.env.NODE_ENV === 'production'
+
 module.exports = {
   // Webpack 5 is enabled by default
   // You can still use webpack 4 while upgrading to the latest version of Next.js by adding the "webpack5: false" flag
+  assetPrefix: isProduction ? '/portfolio/' : '',
 }
