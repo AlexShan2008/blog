91b4edc57545a1ae0b2ecc5367653e9479659d06
Alex Shan <shantong.sir@gmail.com>
2021-11-01 23:53:31 +0800

    feat: add Resume

--- pages/resume/index.tsx
+++ pages/resume/index.tsx
@@ -1,6 +1,20 @@
+import * as React from 'react'
+import AirplanemodeActiveIcon from '@mui/icons-material/AirplanemodeActive'
+import DirectionsBikeIcon from '@mui/icons-material/DirectionsBike'
+import LaptopMacIcon from '@mui/icons-material/LaptopMac'
+import SchoolIcon from '@mui/icons-material/School'
+import TwoWheelerIcon from '@mui/icons-material/TwoWheeler'
+import WorkIcon from '@mui/icons-material/Work'
+import Timeline from '@mui/lab/Timeline'
+import TimelineConnector from '@mui/lab/TimelineConnector'
+import TimelineContent from '@mui/lab/TimelineContent'
+import TimelineDot from '@mui/lab/TimelineDot'
+import TimelineItem from '@mui/lab/TimelineItem'
+import TimelineOppositeContent from '@mui/lab/TimelineOppositeContent'
+import TimelineSeparator from '@mui/lab/TimelineSeparator'
+import Typography from '@mui/material/Typography'
 import Head from 'next/head'
 import { getAllPosts } from '../../lib/api'
-import { CMS_NAME } from '../../lib/constants'
 import Post from '../../types/post'
 import Container from '@/components/container'
 import Layout from '@/components/layout'
@@ -16,9 +30,183 @@ const Resume = ({ allPosts }: Props) => {
     <>
       <Layout>
         <Head>
-          <title>AlexShan&apos;s Blog</title>
+          <title>AlexShan&apos;s Blog - Resume</title>
         </Head>
-        <Container>Resume</Container>
+        <Container>
+          <Timeline position="alternate">
+            <TimelineItem>
+              <TimelineOppositeContent
+                sx={{ m: 'auto 0' }}
+                align="right"
+                variant="body2"
+                color="text.secondary"
+              >
+                Now
+              </TimelineOppositeContent>
+              <TimelineSeparator>
+                <TimelineConnector />
+                <TimelineDot>
+                  <AirplanemodeActiveIcon />
+                </TimelineDot>
+                <TimelineConnector />
+              </TimelineSeparator>
+              <TimelineContent sx={{ py: '12px', px: 2 }}>
+                <Typography variant="h5" component="span">
+                  Senior Front-end
+                </Typography>
+                <Typography>Rightcapital</Typography>
+              </TimelineContent>
+            </TimelineItem>
+
+            <TimelineItem>
+              <TimelineOppositeContent
+                sx={{ m: 'auto 0' }}
+                align="right"
+                variant="body2"
+                color="text.secondary"
+              >
+                2020
+              </TimelineOppositeContent>
+              <TimelineSeparator>
+                <TimelineConnector />
+                <TimelineDot>
+                  <TwoWheelerIcon />
+                </TimelineDot>
+                <TimelineConnector />
+              </TimelineSeparator>
+              <TimelineContent sx={{ py: '12px', px: 2 }}>
+                <Typography variant="h5" component="span">
+                  Head of Front End
+                </Typography>
+                <Typography>Beijing Reyun Technology Co., Ltd.</Typography>
+              </TimelineContent>
+            </TimelineItem>
+
+            <TimelineItem>
+              <TimelineOppositeContent
+                sx={{ m: 'auto 0' }}
+                align="right"
+                variant="body2"
+                color="text.secondary"
+              >
+                2018
+              </TimelineOppositeContent>
+              <TimelineSeparator>
+                <TimelineConnector />
+                <TimelineDot>
+                  <DirectionsBikeIcon />
+                </TimelineDot>
+                <TimelineConnector />
+              </TimelineSeparator>
+              <TimelineContent sx={{ py: '12px', px: 2 }}>
+                <Typography variant="h5" component="span">
+                  Senior Front-end
+                </Typography>
+                <Typography>Keep on learning</Typography>
+              </TimelineContent>
+            </TimelineItem>
+
+            <TimelineItem>
+              <TimelineOppositeContent
+                sx={{ m: 'auto 0' }}
+                variant="body2"
+                color="text.secondary"
+              >
+                2015
+              </TimelineOppositeContent>
+              <TimelineSeparator>
+                <TimelineConnector />
+                <TimelineDot color="primary">
+                  <LaptopMacIcon />
+                </TimelineDot>
+                <TimelineConnector />
+              </TimelineSeparator>
+              <TimelineContent sx={{ py: '12px', px: 2 }}>
+                <Typography variant="h5" component="span">
+                  Frontend Developer
+                </Typography>
+                <Typography>
+                  The beginning of my full-time front-end developer
+                </Typography>
+              </TimelineContent>
+            </TimelineItem>
+
+            <TimelineItem>
+              <TimelineOppositeContent
+                sx={{ m: 'auto 0' }}
+                variant="body2"
+                color="text.secondary"
+              >
+                2013
+              </TimelineOppositeContent>
+              <TimelineSeparator>
+                <TimelineConnector sx={{ bgcolor: 'secondary.main' }} />
+                <TimelineDot color="secondary">
+                  <SchoolIcon />
+                </TimelineDot>
+                <TimelineConnector />
+              </TimelineSeparator>
+              <TimelineContent sx={{ py: '12px', px: 2 }}>
+                <Typography variant="h5" component="span">
+                  Postgraduate
+                </Typography>
+                <Typography>Applied Psychology</Typography>
+                <Typography>Institute of Psychology, CAS</Typography>
+              </TimelineContent>
+            </TimelineItem>
+
+            <TimelineItem>
+              <TimelineOppositeContent
+                sx={{ m: 'auto 0' }}
+                variant="body2"
+                color="text.secondary"
+              >
+                2010
+              </TimelineOppositeContent>
+              <TimelineSeparator>
+                <TimelineConnector />
+                <TimelineDot color="primary" variant="outlined">
+                  <WorkIcon />
+                </TimelineDot>
+                <TimelineConnector sx={{ bgcolor: 'secondary.main' }} />
+              </TimelineSeparator>
+              <TimelineContent sx={{ py: '12px', px: 2 }}>
+                <Typography variant="h5" component="span">
+                  Mechanical and Electrical Engineer
+                </Typography>
+                <Typography>
+                  Research Institute of Highway Ministry Transport
+                </Typography>
+              </TimelineContent>
+            </TimelineItem>
+
+            <TimelineItem>
+              <TimelineOppositeContent
+                sx={{ m: 'auto 0' }}
+                variant="body2"
+                color="text.secondary"
+              >
+                2008
+              </TimelineOppositeContent>
+              <TimelineSeparator>
+                <TimelineConnector sx={{ bgcolor: 'secondary.main' }} />
+                <TimelineDot color="secondary">
+                  <SchoolIcon />
+                </TimelineDot>
+                <TimelineConnector />
+              </TimelineSeparator>
+              <TimelineContent sx={{ py: '12px', px: 2 }}>
+                <Typography variant="h5" component="span">
+                  Bachelor&apos;s degree
+                </Typography>
+                <Typography>Mechanical Engineering and Automation</Typography>
+                <Typography>
+                  Beijing Institute of Graphic Communication
+                </Typography>
+              </TimelineContent>
+            </TimelineItem>
+          </Timeline>
+        </Container>
       </Layout>
     </>
   )
