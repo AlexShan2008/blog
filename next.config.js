f4bbc69a8b7f2d85276824fb376107c6a5d045db
Alex Shan <shantong.sir@gmail.com>
2022-04-12 22:27:56 +0800

    build: use SWC replacing Terser for minification

--- next.config.js
+++ next.config.js
@@ -4,4 +4,5 @@ module.exports = {
   // Webpack 5 is enabled by default
   // You can still use webpack 4 while upgrading to the latest version of Next.js by adding the "webpack5: false" flag
   // assetPrefix: isProduction ? '/portfolio/' : '',
+  swcMinify: true,
 }
